e7fe5fd02abdc68a1333521ab6cfbb8524fbce44
Aaron Oneal <aaron.oneal@spicypixel.com>
2016-06-02 22:14:00 -0700

    Fix TypeScriptBuilder export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 export { default as ChildProcess, SpawnOptions } from "./child-process";
-export { default as TypeScriptBuilder, TypeScriptCompileTaskOptions } from "./typescript-builder";
+export { default as TypeScriptBuilder, TypeScriptBuildOptions } from "./typescript-builder";
 export { default as DoxygenBuilder } from "./doxygen-builder";
 export { default as MonoDocBuilder } from "./monodoc-builder";
 export { default as MochaRunner } from "./mocha-runner";
