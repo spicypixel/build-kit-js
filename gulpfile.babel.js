7bae74222a0c5398aa418664c37f9b1aeb77444f
Aaron Oneal <aaron.oneal@spicypixel.com>
2016-06-02 18:00:53 -0700

    Simplify gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,29 +1,20 @@
-"use strict";
-
-// Tools
-// import 'babel-polyfill'
 import gulp from "gulp";
 import gutil from "gulp-util";
 import mocha from "gulp-mocha";
-import TypeScriptBuilder from "./src/lib/typescript-builder";
-
-// Streams and process
-import fs from "fs-extra";
 import del from "del";
-
-// Handle errors
-gulp.on("error", function (err) {
-  console.log(err);
-  process.exit(-1);
-});
+import TypeScriptBuilder from "./src/lib/typescript-builder";
 
 function clean() {
   return del(["lib", "test", "test-output"]);
 }
 
 async function build() {
-  await clean();
   await TypeScriptBuilder.compileAsync();
+}
+
+async function rebuild() {
+  await clean();
+  await build();
 }
 
 async function test() {
@@ -40,4 +31,5 @@ async function test() {
 gulp.task("default", () => test());
 gulp.task("test", () => test());
 gulp.task("build", () => build());
+gulp.task("rebuild", () => rebuild());
 gulp.task("clean", () => clean());
