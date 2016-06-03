baabba59129f454c3bf92e5f2f94a8df8d942501
Aaron Oneal <aaron.oneal@spicypixel.com>
2016-06-02 21:53:51 -0700

    Add MochaRunner to index

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,4 @@ export { default as ChildProcess, SpawnOptions } from "./child-process";
 export { default as TypeScriptBuilder, TypeScriptCompileTaskOptions } from "./typescript-builder";
 export { default as DoxygenBuilder } from "./doxygen-builder";
 export { default as MonoDocBuilder } from "./monodoc-builder";
+export { default as MochaRunner } from "./mocha-runner";
