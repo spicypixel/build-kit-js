bf77f987d23248a7df854c68294a83edeeafd074
Aaron Oneal <aaron.oneal@spicypixel.com>
2016-06-04 22:19:20 -0700

    Export GitRevision

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,5 @@
 export { default as ChildProcess, SpawnOptions } from "./child-process";
+export { default as GitRevision } from "./git-revision";
 export { default as TypeScriptBuilder, TypeScriptBuildOptions } from "./typescript-builder";
 export { default as DoxygenBuilder } from "./doxygen-builder";
 export { default as MonoDocBuilder } from "./monodoc-builder";
